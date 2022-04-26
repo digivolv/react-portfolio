9f77a4511c5d557b68bd87a1ab32c38884999db9
Edwin Ly <edwinjly@gmail.com>
2022-04-26 16:40:52 -0400

    Add additional styling for about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react"
 import AnimatedLetters from "../AnimatedLetters"
 import "./index.scss"
 
 const About = () => {
+  const [letterClass, setLetterClass] = useState("text-animate")
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass("text-animate-hover")
+    }, 3000)
+  }, [])
+
   return (
     <div className="container about-page">
       <div className="text-zone">
         <h1>
           <AnimatedLetters
+            letterClass={letterClass}
             strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
             idx={15}
           />
